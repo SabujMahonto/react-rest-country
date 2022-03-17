53f15bcaaa2a8ab9bd0c1d0ea5b63d3947162286
SabujMahonto <64306095+SabujMahonto@users.noreply.github.com>
2022-03-17 12:44:29 +0600

    rest contry file init

---
+++ src/component/Countries/Countries.js
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from 'react';
+
+const Countries = () => {
+    const [country, setCountry] = useState([])
+    useEffect(()=>{
+        
+    },[])
+    return (
+        <div>
+            <h1>World's All Country List</h1>
+        </div>
+    );
+};
+
+export default Countries;
